071da186164938719b7ce795dd5978fd469dc006
ArthFarias <arthurrafaelfarias@gmail.com>
2022-05-09 16:22:39 -0300

    Exercício de variável

---
+++ modulo1/template_javascript/index.js
@@ -0,0 +1,66 @@
+/* EXERCÍCIOS DE INTERPRATAÇÃO DE CÓDIGO */
+/*let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+/*  console.log(b) -> Vai mostrar 10; 
+    console.log(a, b) -> Vai mostrar 10 e 5; */
+
+
+
+
+/*let a = 10
+let b = 20
+c = a
+b = c
+a = b
+console.log(a, b, c)
+/*  console.log(a, b, c) -> Vai mostrar 10, 10, 10; */
+
+
+
+
+/*let p = prompt('Quantas horas você trabalha por dia?')
+let t = prompt('Quanto você recebe por dia?')
+alert(`Voce recebe ${t / p} por hora`)
+/*  (p) -> horasTrabalhadas;
+    (t) -> salarioDia; */
+
+
+
+/* EXERCÍCIOD DE ESCRITA DE CÓDIGO */
+
+/*let nome
+let idade
+console.log(typeof nome, idade)
+/*  Aparece undefined porque não atribuiu valor; */
+
+
+
+
+/*let nome2 = prompt('Qual seu nome?')
+let idade2 = prompt('Qual sua idade?')
+
+
+
+console.log('Ola', nome2, 'você tem', idade2, 'anos') */
+
+
+
+
+/*let exercicio = prompt('Você faz exercícios? S/N')
+let estudo = prompt('Você estuda? S/N')
+let trabalho = prompt('Você trabalha? S/N') */
+
+
+
+
+/*let a = 10
+let b = 25
+let c = a
+
+console.log('O novo valor de a é de', b)
+console.log('O novo valor de b é de', c) /*
