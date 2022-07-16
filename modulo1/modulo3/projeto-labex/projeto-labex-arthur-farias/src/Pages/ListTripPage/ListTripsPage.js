4bc095a267e99489aa23de5ed78e8713421cd1f8
ArthFarias <arthurrafaelfarias@gmail.com>
2022-07-15 23:29:11 -0300

    Projeto labex sexta

--- modulo1/modulo3/projeto-labex/projeto-labex-arthur-farias/src/Pages/ListTripPage/ListTripsPage.js
+++ modulo1/modulo3/projeto-labex/projeto-labex-arthur-farias/src/Pages/ListTripPage/ListTripsPage.js
@@ -1,9 +1,11 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { DivPrincipal, DivBotão } from './ListTripsPage.styled'
+import { DivPrincipal, DivBotão, CardListas } from './ListTripsPage.styled'
 import axios from 'axios'
 
 const ListTripsPage = () => {
+	const [listaViagens, setListaViagens] = useState()
+
 	const navigate = useNavigate()
 
 	const goToBack = () => {
@@ -15,16 +17,30 @@ const ListTripsPage = () => {
 	useEffect(() => {
 		axios
 			.get(
-				'https://us-central1-labenu-apis.cloudfunctions.net/labeX/arthur/trips'
+				'https://us-central1-labenu-apis.cloudfunctions.net/labeX/darvas/trips'
 			)
 			.then((res) => {
-				console.log(res)
+				setListaViagens(res.data.trips)
+				console.log(res.data)
 			})
 			.catch((err) => {
 				console.log(err)
 			})
 	}, [])
-
+	const listaMapeada = listaViagens?.map((trips) => {
+		return (
+			<CardListas key={trips.id}>
+				<p>Name: {trips.name}</p>
+				<p>Descrição: {trips.description}</p>
+				<p>Planeta: {trips.planet}</p>
+				<p>Duração: {trips.durationInDays}</p>
+				<p>Data: {trips.date}</p>
+			</CardListas>
+		)
+	})
+	const casoError = () => {
+		return <h2>Lista não encontrada</h2>
+	}
 	return (
 		<DivPrincipal>
 			<DivBotão>
@@ -32,6 +48,7 @@ const ListTripsPage = () => {
 				<button onClick={goToCreateTrip}> Inscrever-se</button>
 			</DivBotão>
 			<h1>Lista de viagem</h1>
+			{listaMapeada}
 		</DivPrincipal>
 	)
 }
