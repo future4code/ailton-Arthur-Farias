8759881e2798ff41c3195c3e86e6ce8113bc22ae
ArthFarias <arthurrafaelfarias@gmail.com>
2022-08-17 23:39:36 -0300

    Atividade semanal

---
+++ modulo1/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,28 @@
+export type Produtos = {
+	id: string
+	name: string
+	price: number
+}
+
+export const arrayProdutos: Produtos[] = [
+	{
+		id: '43g89nsd4t9gpasd',
+		name: 'arroz',
+		price: 8,
+	},
+	{
+		id: 'apdiw982jasdopj3',
+		name: 'panetone',
+		price: 30,
+	},
+	{
+		id: 'adjo3j4agjp43dgr',
+		name: 'caixa de som',
+		price: 199,
+	},
+	{
+		id: 'iwhjd3899a9afu0a',
+		name: 'iphone',
+		price: 3999,
+	},
+]
